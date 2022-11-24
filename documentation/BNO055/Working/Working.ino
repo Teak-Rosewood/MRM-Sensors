73639be74bd58a07201a2ec291ca74a715172406
Saatwik Vasishtha <88363398+Teak-Rosewood@users.noreply.github.com>
2022-11-25 00:43:41 +0530

    Update Working.ino

--- documentation/BNO055/Working/Working.ino
+++ documentation/BNO055/Working/Working.ino
@@ -2,55 +2,113 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <EEPROM.h>
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup(void) 
 {
-  // boud rate = 9600
-  
   Serial.begin(9600);
-  Serial.println("Setting up BNO055"); Serial.println("");
-  
-  /* Initialise the sensor */
-  
-  if(!bno.begin(bno.OPERATION_MODE_NDOF_FMC_OFF))
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  
-  // Setting offsets 
-  
-  uint8_t offsets[22];
-  offsets[0] = 242;
-  offsets[1] = 255;
-  offsets[2] = 1;
-  offsets[3] = 0;
-  offsets[4] = 229;
-  offsets[5] = 255;
-  offsets[6] = 40;
-  offsets[7] = 0;
-  offsets[8] = 184;
-  offsets[9] = 255;
-  offsets[10] = 224;
-  offsets[11] = 253;
-  offsets[12] = 255;
-  offsets[13] = 255;
-  offsets[14] = 0;
-  offsets[15] = 0;
-  offsets[16] = 0;
-  offsets[17] = 0;
-  offsets[18] = 232;
-  offsets[19] = 3;
-  offsets[20] = 188;
-  offsets[21] = 3;
-  
-  bno.setSensorOffsets(&offsets[0]);
-  delay(1000);
-  
-  bno.setExtCrystalUse(true);
+    delay(1000);
+    Serial.println("Orientation Sensor Test"); Serial.println("");
+
+    /* Initialise the sensor */
+    if (!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while (1);
+    }
+
+    int eeAddress = 0;
+    long bnoID;
+    bool foundCalib = false;
+
+    EEPROM.get(eeAddress, bnoID);
+
+    adafruit_bno055_offsets_t calibrationData;
+    sensor_t sensor;
+
+    /*
+    *  Look for the sensor's unique ID at the beginning oF EEPROM.
+    *  This isn't foolproof, but it's better than nothing.
+    */
+    bno.getSensor(&sensor);
+    if (bnoID != sensor.sensor_id)
+    {
+        Serial.println("\nNo Calibration Data for this sensor exists in EEPROM");
+        delay(500);
+    }
+    else
+    {
+        Serial.println("\nFound Calibration for this sensor in EEPROM.");
+        eeAddress += sizeof(long);
+        EEPROM.get(eeAddress, calibrationData);
+
+        Serial.println("\n\nRestoring Calibration data to the BNO055...");
+        bno.setSensorOffsets(calibrationData);
+
+        Serial.println("\n\nCalibration data loaded into BNO055");
+        foundCalib = true;
+    }
+
+    delay(1000);
+
+   /* Crystal must be configured AFTER loading calibration data into BNO055. */
+    bno.setExtCrystalUse(true);
+
+    sensors_event_t event;
+    bno.getEvent(&event);
+    /* always recal the mag as It goes out of calibration very often */
+    if (foundCalib){
+        Serial.println("Move sensor slightly to calibrate magnetometers");
+        while (!bno.isFullyCalibrated())
+        {
+            bno.getEvent(&event);
+        }
+    }
+    else
+    {
+        Serial.println("Please Calibrate Sensor: ");
+        while (!bno.isFullyCalibrated())
+        {
+            bno.getEvent(&event);
+
+            Serial.print("X: ");
+            Serial.print(event.orientation.x, 4);
+            Serial.print("\tY: ");
+            Serial.print(event.orientation.y, 4);
+            Serial.print("\tZ: ");
+            Serial.print(event.orientation.z, 4);
+
+            /* Optional: Display calibration status */
+
+            /* New line for the next sample */
+            Serial.println("");
+
+            /* Wait the specified delay before requesting new data */
+        }
+    }
+
+    Serial.println("\nFully calibrated!");
+    Serial.println("--------------------------------");
+    Serial.println("Calibration Results: ");
+    adafruit_bno055_offsets_t newCalib;
+    bno.getSensorOffsets(newCalib);
+    Serial.println("\n\nStoring calibration data to EEPROM...");
+
+    eeAddress = 0;
+    bno.getSensor(&sensor);
+    bnoID = sensor.sensor_id;
+
+    EEPROM.put(eeAddress, bnoID);
+
+    eeAddress += sizeof(long);
+    EEPROM.put(eeAddress, newCalib);
+    Serial.println("Data stored to EEPROM.");
+
+    Serial.println("\n--------------------------------\n");
+    delay(500);
 
 }
 
